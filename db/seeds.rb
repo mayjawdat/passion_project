02b933e58836c8e4af6953cb887524d07945a566
May Jawdat <may.jawdat@gmail.com>
2017-04-09 19:37:28 -0700

    Update seed file to prevent db from saving unencrypted password

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ end
 people = []
 
 15.times do 
-  people << User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, hashed_password: Faker::Internet.password(8, 10))
+  people << User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password(8, 10))
 end
 
 40.times do
