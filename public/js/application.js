0cc484d090b336d7e5945a863028252e59c581c6
May Jawdat <may.jawdat@gmail.com>
2017-04-14 17:38:41 -0700

    Sort out cat index view

--- public/js/application.js
+++ public/js/application.js
@@ -33,10 +33,8 @@ var returnCatHandler = function(){
 
 // Next slide
 $('.carousel').carousel('next');
-$('.carousel').carousel('next', 3); // Move next n times.
 // Previous slide
 $('.carousel').carousel('prev');
-$('.carousel').carousel('prev', 4); // Move prev n times.
-// Set to nth slide
-$('.carousel').carousel('set', 4);
+
+
       
